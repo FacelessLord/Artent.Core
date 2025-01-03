8bafd0a5773cafafb29db5948e530903deeaf3ea
faceless <skyres21@yandex.ru>
2025-01-03 14:41:38 +0500

    Artent -> ArtentCore

--- src/main/java/faceless/artent_core/ArtentCore.java
+++ src/main/java/faceless/artent_core/ArtentCore.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Artent implements ModInitializer {
+public class ArtentCore implements ModInitializer {
     public static final String MODID = "artent_core";
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
